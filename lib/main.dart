18046541b3bf51f2dfd9a5125f2ce9977dfe2f20
lebao3105 <bao12345yocoo@gmail.com>
2024-08-18 00:09:54 +0700

    Finally fixed navigation stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,66 @@
 import 'dart:io';
 
 import 'package:fluent_ui/fluent_ui.dart';
-import 'package:onstorage/UI/AboutView.dart';
-import 'package:onstorage/UI/LanguagesView.dart';
-import 'package:onstorage/UI/Utilities.dart';
 import 'package:system_theme/system_theme.dart';
 
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'l10n/app_localizations.dart';
 
 import 'package:flutter/foundation.dart' show kIsWeb;
-import 'package:permission_handler/permission_handler.dart';
+//import 'package:permission_handler/permission_handler.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:go_router/go_router.dart';
 import 'package:window_manager/window_manager.dart';
 
-import 'package:onstorage/Home.dart';
-import 'package:onstorage/QueueLists.dart';
-import 'package:onstorage/Settings.dart';
-import 'package:onstorage/DirView.dart';
+import 'package:onstorage/UI/AboutView.dart';
+import 'package:onstorage/UI/LanguagesView.dart';
+import 'package:onstorage/UI/Utilities.dart';
+import 'package:onstorage/UI/Home.dart';
+import 'package:onstorage/UI/QueueLists.dart';
+import 'package:onstorage/UI/Settings.dart';
+import 'package:onstorage/UI/DirView.dart';
 import 'package:onstorage/Utilities.dart';
 
 
-	final shellNavKey = GlobalKey<NavigatorState>();
-	final rootNavKey = GlobalKey<NavigatorState>();
-	final _router = GoRouter(routes: [
+final shellNavKey = GlobalKey<NavigatorState>();
+final rootNavKey = GlobalKey<NavigatorState>();
+final _router = GoRouter(
+	navigatorKey: rootNavKey,
+	initialLocation: '/',
+	routes: [
 		ShellRoute(
-			builder: (_, __, ___) => const NavView(),
+			builder: (_, __, child) => NavView(child: child),
 			navigatorKey: shellNavKey,
 			routes: [
 				GoRoute(
 					path: '/',
-					builder: (ctxt, state) { changes.navSelectedIdx = 0; return HomePage(); }
+					builder: (ctxt, state) => HomePage()
 				),
 				GoRoute(
 					path: '/settings',
-					builder: (ctxt, state) => SettingsPage(),
+					builder: (ctxt, state) => SettingsPage()
 				),
 				GoRoute(
 					path: '/settings/languages',
 					builder: (ctxt, state) => LanguagesPage()
 				),
 				GoRoute(
-					path: '/queue',
-					builder: (_, state) => QueueList(type: QueueType.getByString(state.pathParameters['which']!))
-				),
-				GoRoute(
 					path: '/view',
-					builder: (_, state) => DirView(where: state.pathParameters['where'] ?? homePath() ?? '/')
+					builder: (_, state) {
+						return DirView(where: state.uri.queryParameters['where'] ?? homePath() ?? '/');
+					}
 				),
 				GoRoute(
 					path: '/about',
 					builder: (_, __) => AboutPage()
 				)
-			]
+			] + QueueType.values.map(
+				(e) => GoRoute(
+					path: '/queue/${e.name.toLowerCase()}',
+					builder: (_, __) => QueueList(type: e))
+			).toList()
 		)
 	]
-	, navigatorKey: rootNavKey
 );
 
 class MainApp extends StatelessWidget {
@@ -73,11 +77,11 @@ class MainApp extends StatelessWidget {
 				[
 					Button(
 						child: const Text('Ok, just let me go around'),
-						onPressed: () { context.pop(); }
+						onPressed: () => context.pop()
 					),
 					Button(
 						child: const Text('Quit'),
-						onPressed: () { exit(0); }
+						onPressed: () => exit(0)
 					)
 				]
 			);
@@ -105,191 +109,164 @@ class MainApp extends StatelessWidget {
 				FluentLocalizations.delegate
 			],
 			supportedLocales: AppLocalizations.supportedLocales,
-			locale: AppLocalizations.supportedLocales[selectedLanguage]
-		);
-	}
-}
-
-
-class _NavView extends State<NavView>
-{
-	
-	@override
-	Widget build(BuildContext context)
-	{
-		final loc = AppLocalizations.of(context)!;
-
-		final footerItems = [
-			PaneItem(
-				icon: const Icon(FluentIcons.settings),
-				title: createText(loc.settings),
-				body: SettingsPage(),
-			),
-			PaneItem(
-				icon: const Icon(FluentIcons.info),
-				title: createText(loc.settings_about),
-				body: AboutPage(),
-			)
-		];
-
-		final navItems = [
-			PaneItem(
-				icon: const Icon(FluentIcons.home),
-				title: createText(loc.home),
-				body: HomePage()
-			),
-			PaneItem(
-				icon: const Icon(FluentIcons.folder),
-				title: createText('Folder'),
-				body: DirView(where: 'C:\\Windows\\System32')
-			),
-			PaneItemHeader(header: createText(loc.queue)),
-			PaneItem(
-				icon: const Icon(FluentIcons.copy),
-				title: createText(loc.copy),
-				body: const QueueList(type: QueueType.Copy),
-				infoBadge: InfoBadge(source: Text(copyQueue.length.toString()))
-			),
-
-			PaneItem(
-				icon: const Icon(FluentIcons.move),
-				title: createText(loc.move),
-				body: const QueueList(type: QueueType.Move),
-				infoBadge: InfoBadge(source: Text(moveQueue.length.toString()))
-			),
-
-			PaneItem(
-				icon: const Icon(FluentIcons.delete),
-				title: createText(loc.trash),
-				body: const QueueList(type: QueueType.Delete),
-				infoBadge: InfoBadge(source: Text(delQueue.length.toString()))
-			)
-		];
-
-		return ListenableBuilder(
-			listenable: changes,
-			builder: (ctxt, _) => NavigationView(
-				appBar: NavigationAppBar(
-          automaticallyImplyLeading: false,
-          leading: null,
-					title: null,
-					actions: WindowCaption(
-						title:
-							changes.showDirBar ? CommandBar(
-								overflowBehavior: CommandBarOverflowBehavior.noWrap,
-								primaryItems: [
-									
-									// Back
-									CommandBarBuilderItem(
-										builder: (_, __, w) => Tooltip(message: 'Go back', child: w),
-										wrappedItem: CommandBarButton(
-											icon: const Icon(FluentIcons.back),
-											onPressed: () {}
-										)
-									),
-
-									// Forward
-									CommandBarBuilderItem(
-										builder: (_, __, w) => Tooltip(message: 'Go forward', child: w),
-										wrappedItem: CommandBarButton(
-											icon: const Icon(FluentIcons.forward),
-											onPressed: () {}
-										)
-									),
-
-									// Go up
-									CommandBarBuilderItem(
-										builder: (_, __, w) => Tooltip(message: 'Go up', child: w),
-										wrappedItem: CommandBarButton(
-											icon: const Icon(FluentIcons.up),
-											onPressed: () => changes.currDir = "${changes.currDir}/.."
-										)
-									),
-
-									// Refresh
-									CommandBarBuilderItem(
-										builder: (_, __, w) => Tooltip(message: 'Refresh', child: w),
-										wrappedItem: CommandBarButton(
-											icon: const Icon(FluentIcons.back),
-											onPressed: () => setState(() {})
-										)
-									),
-
-									// Create button
-									CommandBarBuilderItem(
-										builder: (_, __, w) => Tooltip(message: "Create something new", child: w),
-										wrappedItem: CommandBarButton(
-											icon: const Icon(FluentIcons.add),
-											onPressed: () {
-												String input = "";
-												createDialog(
-													context, 'Create a new file or folder',
-													Column(
-														mainAxisSize: MainAxisSize.min,
-														children: [
-															InfoLabel(
-																label: 'Enter the relative path of the new item to be created',
-																child: TextBox(
-																	expands: false, maxLines: null, autocorrect: false,
-																	showCursor: true, autofocus: true, onChanged: (value) => input = value
-																),
-															)
-														]
-													),
-													[
-														Button(child: createText('New file'), onPressed: () => setState(() => createNew(input, true))),
-														Button(child: createText('New folder'), onPressed: () => setState(() => createNew(input, false))),
-														FilledButton(child: createText('Cancel'), onPressed: () => context.pop())
-													]
-												);
-											}
-										)
-									),
-
-									// Delete button
-									CommandBarBuilderItem(
-										builder: (_, __, w) => Tooltip(message: 'Delete selected item(s)', child: w),
-										wrappedItem: CommandBarButton(
-											icon: const Icon(FluentIcons.delete),
-											onPressed: () {}
-										)
-									),
-
-									// The rest goes here...
-								],
-							) : null
-					)
-
-					/* End Window Caption - NavigationAppBar [actions] */
-				),
-
-				/* End NavigationAppBar - NavigationView [appBar] */
-
-				pane: NavigationPane(
-					displayMode: PaneDisplayMode.auto,
-					items: navItems,
-					footerItems: footerItems,
-					selected: changes.navSelectedIdx,
-					onChanged: (index) => setState(() => changes.navSelectedIdx = index)
-				)
-
-				/* End NavigationPane - NavigationView [pane] */
-			)
-
-			/* End NavigationView */
+			locale: Locale(selectedLanguage)
 		);
 	}
 }
 
 class NavView extends StatefulWidget
 {
-	const NavView({super.key});
+	const NavView({super.key, required this.child});
+
+	final Widget child;
 
 	@override
 	State<StatefulWidget> createState() => _NavView();
 }
 
-void main() async {
+class _NavView extends State<NavView>
+{
+
+	late final loc = AppLocalizations.of(context)!;
+
+	/// Creates a navigation pane item
+	/// Additional callback will be called
+	/// after this.context goes to the target path.
+	PaneItem createNavPaneItem(
+		String title, IconData icon, String path,
+		{Function? additionalCallback, InfoBadge? badge})
+	{
+		return PaneItem(
+			key: Key(path),
+			title: createText(title),
+			icon: Icon(icon),
+			body: const SizedBox.shrink(),
+			onTap: () {
+				if (GoRouterState.of(context).uri.toString() != path)
+					{ context.go(path); }
+				additionalCallback?.call();
+			},
+			infoBadge: badge
+		);
+	}
+
+	/// Finds the current pane index, using the current pane body's URI.
+	int findNavSelectedIndex()
+	{
+		final location = GoRouterState.of(context).uri.toString();
+    final allItems = (navItems + footerItems);
+
+		// Who gives key to a PaneItemHeader?
+		// TODO: Tree on pane (PaneItemExpander or smth)
+		allItems.removeWhere(
+			(item) => (item.key == null)
+		);
+
+		/// Note: Key.toString() returns a weird format like this:
+		/// [<'/view?where=/usr/share/applications'>]
+		
+    int ret = allItems.indexWhere(
+			(item) => (item.key == Key(location))
+				|| (item.key.toString().startsWith('[<\'/view\'>]'))
+		);
+
+		return (ret != -1) ? ret : 0;
+	}
+
+	late final footerItems = [
+		createNavPaneItem(loc.settings, FluentIcons.settings, '/settings'),
+		createNavPaneItem(loc.settings_about, FluentIcons.info, '/about')
+	];
+
+	late final navItems = [
+		createNavPaneItem(
+			loc.home,
+			FluentIcons.home,
+			'/'
+		),
+		createNavPaneItem(
+			'Folder',
+			FluentIcons.folder,
+			'/view?where=${changes.currDir}'
+		),
+
+		// Separator below
+
+		PaneItemHeader(header: createText(loc.queue)),
+
+		// End separator
+
+		createNavPaneItem(
+			loc.copy,
+			FluentIcons.copy,
+			'/queue/copy',
+			badge: InfoBadge(source: Text(copyQueue.length.toString()))
+		),
+
+		createNavPaneItem(
+			loc.move,
+			FluentIcons.move,
+			'/queue/move',
+			badge: InfoBadge(source: Text(moveQueue.length.toString()))
+		),
+
+		createNavPaneItem(
+			loc.trash,
+			FluentIcons.delete,
+			'/queue/delete',
+			badge: InfoBadge(source: Text(delQueue.length.toString()))
+		)
+	];
+	
+	@override
+	Widget build(BuildContext context)
+	{
+		return ListenableBuilder(
+			listenable: changes,
+			builder: (ctxt, _) =>
+				NavigationView(
+					appBar: NavigationAppBar(
+						automaticallyImplyLeading: false,
+						leading: null,
+						title: null,
+						actions:
+							(Platform.isLinux || Platform.isMacOS || Platform.isWindows)
+								? WindowCaption()
+								: null
+					),
+
+					/* End NavigationAppBar - NavigationView [appBar] */
+
+					paneBodyBuilder: (item, child) {
+						// From FluentUI's example
+						final name =
+            	item?.key is ValueKey ? (item!.key as ValueKey).value : null;
+						return FocusTraversalGroup(
+							key: ValueKey('body$name'),
+							child: widget.child,
+						);
+					},
+
+					/* End NavigationView's paneBodyBuilder */
+
+					pane: NavigationPane(
+						displayMode: PaneDisplayMode.auto,
+						items: navItems,
+						footerItems: footerItems,
+						selected: findNavSelectedIndex()
+					)
+
+					/* End NavigationPane - NavigationView [pane] */
+				)
+
+			/* End NavigationView */
+		);
+	}
+}
+
+
+void main() async
+{
 	WidgetsFlutterBinding.ensureInitialized();
 	await SystemTheme.accentColor.load();
 
@@ -300,7 +277,9 @@ void main() async {
 			const WindowOptions(
 				windowButtonVisibility: false,
 				titleBarStyle: TitleBarStyle.hidden
-			), () async {
+			),
+			
+			() async {
 				await windowManager.show();
 				await windowManager.focus();
 			}
@@ -308,11 +287,11 @@ void main() async {
 	}
 	
 	// ask for storage permissions
-	await Permission.storage.request();
-	await Permission.manageExternalStorage.request();
+//	await Permission.storage.request();
+//	await Permission.manageExternalStorage.request();
 	
 	prefs = await SharedPreferences.getInstance();
-	selectedLanguage = (prefs.getInt('selectedLanguage') ?? 0);
+	selectedLanguage = (prefs.getString('selectedLanguage') ?? 'en');
 	pinned = (prefs.getStringList('pinned') ?? []);
 	favourites = (prefs.getStringList('favourites') ?? []);
 
